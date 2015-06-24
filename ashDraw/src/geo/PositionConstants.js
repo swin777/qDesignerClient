79f60ad6dff3b88977eeb67c28585f41f5084ec3
ash <ash@192.168.0.10>
2015-06-24 23:37:44 +0900

    ..

--- ashDraw/src/geo/PositionConstants.js
+++
@@ -1,7 +0,0 @@
-ashDraw.geo.PositionConstants=function(){
-};
-
-ashDraw.geo.PositionConstants.NORTH =  1;
-ashDraw.geo.PositionConstants.SOUTH =  4;
-ashDraw.geo.PositionConstants.WEST  =  8;
-ashDraw.geo.PositionConstants.EAST  = 16;
